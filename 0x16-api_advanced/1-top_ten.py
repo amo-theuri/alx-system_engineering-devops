17eea4628c45298cf4c3502b915c9c82e839d74c
amo-theuri <ammotheuri@gmail.com>
2024-09-27 06:33:47 -0700

    top ten 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"first 10 hot posts"
-
+""" module for function to return top 10 hot posts of a given subreddit """
 import requests
+import sys
 
 
 def top_ten(subreddit):
+    """ Returns: top ten post titles
+        or None if queried subreddit is invalid """
+    headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    parameters = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
+
     if response.status_code == 200:
-        data = response.json()
-        for i in range(10):
-            print(data['data']['children'][i]['data']['title'])
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
     else:
         print(None)
