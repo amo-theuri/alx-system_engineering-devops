f3e0fa9413ab3a3edfde92f6514701abb9ad3c60
amo-theuri <ammotheuri@gmail.com>
2024-09-27 06:01:34 -0700

    subs 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-""" module for function to return number of subreddit subscribers """
+"subscribers count"
+
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """  Args:
-        subreddit: subreddit name
-    Returns:
-        number of subscribers to the subreddit,
-        or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'xica369'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+    data = response.json()
+    if response.status_code != 200:
+        return 0
+    else:
+        return data['data']['subscribers']
