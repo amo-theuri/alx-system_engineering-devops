679bf2bba5fc19691fa4c07eee697e311c155a1d
amo-theuri <ammotheuri@gmail.com>
2024-09-27 05:34:58 -0700

    debug

--- 0-strace_is_your_friend.pp
+++ 0-strace_is_your_friend.pp
@@ -1,5 +1,10 @@
-# strace - fixed .phpp in wp-settings.php
-exec { 'fixed-phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin';
+# A puppet manuscript to replace a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
